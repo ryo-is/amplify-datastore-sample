ec6c0d42ec04d0550f108120e33b5823546ab7f0
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-12-05 16:03:18 +0900

    feat:query/save/deleteの実装

--- src/views/home/script.ts
+++ src/views/home/script.ts
@@ -1,19 +1,61 @@
 import Vue from "vue"
 import Store from "@/store"
-import { HomeComponentState } from "@/types"
+// import Amplify from "@aws-amplify/core"
+import { DataStore } from "@aws-amplify/datastore"
+import { Post, PostStatus } from "@/models"
 
 export default Vue.extend({
-  data(): HomeComponentState {
+  data() {
     return {
-      text: "hoge"
+      text: "hoge",
+      posts: []
     }
   },
   created() {
     Store.dispatch("setHiddenToolbarItems", true)
   },
+  async mounted() {
+    await this.queryData()
+  },
   methods: {
     getText(): string {
       return this.text
+    },
+    async queryData() {
+      try {
+        this.posts = await DataStore.query(Post)
+        console.log(this.posts)
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    async postData() {
+      try {
+        await DataStore.save(
+          new Post({
+            title: "My First Post",
+            rating: 10,
+            status: PostStatus.ACTIVE
+          })
+        )
+        console.log("Saved Posts")
+        await this.queryData()
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    async deleteData() {
+      try {
+        if (this.posts.length > 0) {
+          await DataStore.delete(this.posts[0])
+          console.log("Deleted Posts")
+          await this.queryData()
+        } else {
+          console.log("No Posts")
+        }
+      } catch (err) {
+        console.error(err)
+      }
     }
   }
 })
